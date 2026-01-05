5e8a27b89d1331b464f761ec3b9314c9c89e82d6
Dineshkumar2172 <dinesh2000.data@gmail.com>
2026-01-05 23:33:09 +0530

    linked index view with detail view

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -22,5 +22,5 @@ from shop import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
-    path("detail/", views.detail, name="detail"),
+    path("<int:id>/", views.detail, name="detail"),
 ]
